06b436c52f0718cc724414cfbcbcca6e6c2f6598
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-29 00:26:36 +0300

    EC-1: added phone controller.

---
+++ ElectronicCard/EC.Web/Controllers/PhoneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using EC.BusinessLogic.Services.Interfaces;
+
+namespace EC.Web.Controllers
+{
+    public class PhoneController : Controller
+    {
+        private readonly IPhoneService _phoneService;
+
+        public PhoneController(IPhoneService phoneService)
+        {
+            _phoneService = phoneService;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_phoneService.GetPhones());
+        }
+    }
+}
