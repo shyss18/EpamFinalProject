df8133d507a0a7c9102d498b221180448ece6ed5
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-29 00:19:29 +0300

    EC-1: created phone service.

---
+++ ElectronicCard/EC.BusinessLogic/Services/Interfaces/IPhoneService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using EC.Entities.Entities;
+
+namespace EC.BusinessLogic.Services.Interfaces
+{
+    public interface IPhoneService
+    {
+        IEnumerable<Phone> GetPhones();
+    }
+}
