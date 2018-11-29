a559f633db2b5a873f1051b8d6bf3275a3ec13da
Sergey Petrochenko <proffsneip@gmail.com>
2018-11-30 01:04:17 +0300

    EC-2: updated entites.

--- ElectronicCard/EC.Entities/Entities/User.cs
+++ ElectronicCard/EC.Entities/Entities/User.cs
@@ -14,6 +14,12 @@ namespace EC.Entities.Entities
 
         public string LastName { get; set; }
 
+        public string Login { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
         public DateTime DateBirth { get; set; }
 
         public IReadOnlyCollection<Phone> PhoneNumbers { get; set; }
