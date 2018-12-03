d4925c05feee83b4b3cf7addcee78318452ee0b3
Sergey Petrochenko <proffsneip@gmail.com>
2018-12-03 09:26:59 +0300

    EC-2: addedd stored procedure for create user.

---
+++ DataBase/DataBaseScripts/StoredProcedures/Users/CreateUser.sql
@@ -0,0 +1,29 @@
+USE ElectronicCardDB;
+GO
+
+CREATE PROC CreateUser
+    @login NVARCHAR(20),
+	@password NVARCHAR(20),
+	@email NVARCHAR(30),
+	@isDoctor BIT,
+	@roleId INT,
+	@firstName NVARCHAR(20),
+	@middleName NVARCHAR(20),
+	@lastName NVARCHAR(20),
+	@dateBirth DATE,
+	@work NVARCHAR(40),
+	@photoPath NVARCHAR(50)
+AS
+BEGIN
+INSERT Users(Login, Password, Email, IsDoctor, RoleId)
+VALUES (@login, @password, @email, @isDoctor, @roleId)
+DECLARE @userId INT;
+SET @userId = (SELECT SCOPE_IDENTITY());
+
+IF @isDoctor != 0
+INSERT Doctors(FirstName, MiddleName, LastName, DateBirth, Position, PhotoPath, UserId)
+VALUES(@firstName, @middleName, @lastName, @dateBirth, @work, @photoPath, @userId)
+ELSE
+INSERT Doctors(FirstName, MiddleName, LastName, DateBirth, Position, PhotoPath, UserId)
+VALUES(@firstName, @middleName, @lastName, @dateBirth, @work, @photoPath, @userId)
+END;
