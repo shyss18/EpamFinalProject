8101aaf54305a1dc96fac8036eb00e77589bd093
Sergey Petrochenko <proffsneip@gmail.com>
2018-12-09 14:01:57 +0300

    EC-4: updated helper for create parameter.

---
+++ ElectronicCard/EC.DataAccess/Helpers/Interface/ICreateParameterHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace EC.DataAccess.Helpers.Interface
+{
+    public interface ICreateParameterHelper
+    {
+        IDataParameter CreateParameter(string name, object value, DbType type);
+    }
+}
