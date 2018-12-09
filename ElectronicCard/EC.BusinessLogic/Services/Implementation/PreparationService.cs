b7c7f83556f6ce736e3896d6be2a05fb3c9eb11d
Sergey Petrochenko <proffsneip@gmail.com>
2018-12-09 14:06:38 +0300

    EC-4: create preparation services.

---
+++ ElectronicCard/EC.BusinessLogic/Services/Implementation/PreparationService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using EC.BusinessLogic.Services.Interfaces;
+using EC.DataAccess.Repositories.Interfaces;
+using EC.Entities.Entities;
+
+namespace EC.BusinessLogic.Services.Implementation
+{
+    public class PreparationService : IPreparationService
+    {
+        private readonly IPreparationRepository _preparationRepository;
+
+        public PreparationService(IPreparationRepository preparationRepository)
+        {
+            _preparationRepository = preparationRepository;
+        }
+
+        public void CreatePreparation(Preparation preparation)
+        {
+            _preparationRepository.Create(preparation);
+        }
+
+        public void UpdatePreparation(Preparation preparation)
+        {
+            _preparationRepository.Update(preparation);
+        }
+
+        public void DeletePreparation(int? id)
+        {
+            _preparationRepository.Delete(id);
+        }
+
+        public Preparation GetById(int? id)
+        {
+            return id == null ? null : _preparationRepository.GetById(id);
+        }
+
+        public IReadOnlyCollection<Preparation> GetAll()
+        {
+            return _preparationRepository.GetAll();
+        }
+    }
+}
---
+++ ElectronicCard/EC.BusinessLogic/Services/Interfaces/IPreparationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using EC.Entities.Entities;
+
+namespace EC.BusinessLogic.Services.Interfaces
+{
+    public interface IPreparationService
+    {
+        void CreatePreparation(Preparation preparation);
+        void UpdatePreparation(Preparation preparation);
+        void DeletePreparation(int? id);
+        Preparation GetById(int? id);
+        IReadOnlyCollection<Preparation> GetAll();
+    }
+}
