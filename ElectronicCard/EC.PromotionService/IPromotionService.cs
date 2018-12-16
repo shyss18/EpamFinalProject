4af590b5d8741a011e50b2cada4be7dc2e9f1095
Sergey Petrochenko <proffsneip@gmail.com>
2018-12-16 20:43:49 +0300

    EC-6: added wcf service in project.

---
+++ ElectronicCard/EC.PromotionService/IPromotionService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using EC.Entities.Entities;
+
+namespace EC.PromotionService
+{
+    [ServiceContract]
+    public interface IPromotionService
+    {
+        [OperationContract]
+        string TestConnection();
+
+        [OperationContract]
+        void CreatePromotion(Promotion item);
+
+        [OperationContract]
+        Promotion GetByIdPromotion(int? id);
+
+        [OperationContract]
+        IReadOnlyCollection<Promotion> GetAllPromotions();
+
+        [OperationContract]
+        void DeletePromotion(int? id);
+
+        [OperationContract]
+        void UpdatePromotion(int? id);
+    }
+}
