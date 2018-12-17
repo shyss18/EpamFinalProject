a5dc2bf2ae454951c43507d562ea51d48cf2082f
Sergey Petrochenko <proffsneip@gmail.com>
2018-12-17 23:45:48 +0300

    EC-4: update sickleave repository.

--- ElectronicCard/EC.DataAccess/Repositories/Implementation/SickLeaveRepository.cs
+++ ElectronicCard/EC.DataAccess/Repositories/Implementation/SickLeaveRepository.cs
@@ -1,7 +1,6 @@
 ﻿using EC.DataAccess.Helpers.Interface;
 using EC.DataAccess.Repositories.Interfaces;
 using EC.Entities.Entities;
-using System;
 using System.Collections.Generic;
 using System.Data;
 
@@ -18,10 +17,10 @@ namespace EC.DataAccess.Repositories.Implementation
 
         public void Create(SickLeave item)
         {
-            var isGiveParameter = _query.CreateParameter("isGive", item.IsGive, DbType.Binary);
+            var isGiveParameter = _query.CreateParameter("isGive", item.IsGive, DbType.Boolean);
             var numberParameter = _query.CreateParameter("number", item.Number, DbType.Int32);
             var periodParameter = _query.CreateParameter("periodAction", item.PeriodAction, DbType.Int32);
-            var diagnosisParameter = _query.CreateParameter("diagnosisId", item.Diagnosis.Id, DbType.Int32);
+            var diagnosisParameter = _query.CreateParameter("diagnosisId", item.DiagnosisId, DbType.Int32);
 
             _query.CreateConnection()
                 .CreateCommand(DbConstants.CREATE_SICKLEAVE)
@@ -32,10 +31,10 @@ namespace EC.DataAccess.Repositories.Implementation
         public void Update(SickLeave item)
         {
             var idParameter = _query.CreateParameter("id", item.Id, DbType.Int32);
-            var isGiveParameter = _query.CreateParameter("isGive", item.IsGive, DbType.Binary);
+            var isGiveParameter = _query.CreateParameter("isGive", item.IsGive, DbType.Boolean);
             var numberParameter = _query.CreateParameter("number", item.Number, DbType.Int32);
             var periodParameter = _query.CreateParameter("periodAction", item.PeriodAction, DbType.Int32);
-            var diagnosisParameter = _query.CreateParameter("diagnosisId", item.Diagnosis.Id, DbType.Int32);
+            var diagnosisParameter = _query.CreateParameter("diagnosisId", item.DiagnosisId, DbType.Int32);
 
             _query.CreateConnection()
                 .CreateCommand(DbConstants.UPDATE_SICKLEAVE)
@@ -55,12 +54,61 @@ namespace EC.DataAccess.Repositories.Implementation
 
         public SickLeave GetById(int? id)
         {
-            throw new NotImplementedException();
+            var idParameter = _query.CreateParameter("id", id, DbType.Int32);
+
+            var reader = _query.CreateConnection()
+                .CreateCommand(DbConstants.GET_SICKLEAVE_BY_ID)
+                .AddParameters(idParameter)
+                .ExecuteReader();
+
+            SickLeave sickLeave = null;
+
+            foreach (var item in reader)
+            {
+                sickLeave = new SickLeave
+                {
+                    Id = (int)item["SickLeaveId"],
+                    IsGive = (bool)item["IsGive"],
+                    Number = (int)item["Number"],
+                    PeriodAction = (int)item["PeriodAction"],
+                    DiagnosisId = (int)item["DiagnosisId"],
+                    Diagnosis = new Diagnosis
+                    {
+                        Id = (int)item["DiagnosisId"],
+                        Title = (string)item["Title"]
+                    }
+                };
+            }
+
+            return sickLeave;
         }
 
         public IReadOnlyCollection<SickLeave> GetAll()
         {
-            throw new NotImplementedException();
+            List<SickLeave> allSickLeaves = new List<SickLeave>();
+
+            var reader = _query.CreateConnection()
+                .CreateCommand(DbConstants.GET_ALL_SICKLEAVES)
+                .ExecuteReader();
+
+            foreach (var item in reader)
+            {
+                allSickLeaves.Add(new SickLeave
+                {
+                    Id = (int)item["SickLeaveId"],
+                    IsGive = (bool)item["IsGive"],
+                    Number = (int)item["Number"],
+                    PeriodAction = (int)item["PeriodAction"],
+                    DiagnosisId = (int)item["DiagnosisId"],
+                    Diagnosis = new Diagnosis
+                    {
+                        Id = (int)item["DiagnosisId"],
+                        Title = (string)item["Title"]
+                    }
+                });
+            }
+
+            return allSickLeaves;
         }
     }
 }
