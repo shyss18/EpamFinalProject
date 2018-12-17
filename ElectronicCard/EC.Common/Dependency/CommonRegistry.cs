de554becb549af62c424f8f2a2dde37cc37cc935
Sergey Petrochenko <proffsneip@gmail.com>
2018-12-17 21:08:09 +0300

    EC-7: created logger.

---
+++ ElectronicCard/EC.Common/Dependency/CommonRegistry.cs
@@ -0,0 +1,13 @@
+﻿using EC.Common.Logger;
+using StructureMap;
+
+namespace EC.Common.Dependency
+{
+    public class CommonRegistry : Registry
+    {
+        public CommonRegistry()
+        {
+            For<ICustomLogger>().Singleton().Use<CustomLogger>();
+        }
+    }
+}
--- ElectronicCard/EC.Web/Global.asax.cs
+++ ElectronicCard/EC.Web/Global.asax.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using EC.Common.Logger;
+using EC.Web.DependencyResolution;
 
 namespace EC.Web
 {
@@ -13,6 +12,21 @@ namespace EC.Web
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+
+            var container = IoC.Initialize();
+
+            var logger = container.GetInstance<ICustomLogger>();
+
+            string separatorString = new string('-', 100);
+
+            var exceptionString = string.Format($"EXCEPTION TYPE: {exception.GetType().FullName} \n EXCEPTION MESSAGE: {exception.Message} \n EXCEPTION DLL: {exception.Source} \n EXCEPTION METHOD: {exception.TargetSite.Name} \n EXCEPTION STACKTRACE: {exception.StackTrace} \n {separatorString} \n {string.Empty}");
+
+            logger.WriteToFile(exceptionString);
         }
     }
 }
