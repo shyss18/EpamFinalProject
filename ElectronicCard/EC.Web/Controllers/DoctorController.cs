f181f63ce29960bf65a1be825bd38eb5ac217c5d
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-03 20:47:39 +0300

    EC-8: create controllers for entity.

---
+++ ElectronicCard/EC.Web/Controllers/DoctorController.cs
@@ -0,0 +1,76 @@
+﻿using EC.BusinessLogic.Services.Interfaces;
+using EC.Entities.Entities;
+using System.Web.Mvc;
+
+namespace EC.Web.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+
+        [HttpGet]
+        public ActionResult CreateDoctor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateDoctor(Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                _doctorService.CreateDoctor(doctor);
+            }
+
+            return View(doctor);
+        }
+
+        [HttpGet]
+        public ActionResult UpdateDoctor(int? id)
+        {
+            var doctor = _doctorService.GetDoctorById(id);
+
+            return doctor == null ? View("NotFound") : View(doctor);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateDoctor(Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                _doctorService.UpdateDoctor(doctor);
+            }
+
+            return View(doctor);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            var doctor = _doctorService.GetDoctorById(id);
+
+            return doctor == null ? View("NotFound") : View(doctor);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteDoctor(int? id)
+        {
+            _doctorService.DeleteDoctor(id);
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetAllDoctors()
+        {
+            var doctors = _doctorService.GetAllDoctors();
+
+            return View(doctors);
+        }
+    }
+}
---
+++ ElectronicCard/EC.Web/Controllers/PatientController.cs
@@ -0,0 +1,76 @@
+﻿using EC.BusinessLogic.Services.Interfaces;
+using EC.Entities.Entities;
+using System.Web.Mvc;
+
+namespace EC.Web.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly IPatientService _patientService;
+
+        public PatientController(IPatientService patientService)
+        {
+            _patientService = patientService;
+        }
+
+        [HttpGet]
+        public ActionResult CreatePatient()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePatient(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _patientService.CreatePatient(patient);
+            }
+
+            return View(patient);
+        }
+
+        [HttpGet]
+        public ActionResult UpdatePatient(int? id)
+        {
+            var patient = _patientService.GetPatientById(id);
+
+            return patient == null ? View("NotFound") : View(patient);
+        }
+
+        [HttpPost]
+        public ActionResult UpdatePatient(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _patientService.UpdatePatient(patient);
+            }
+
+            return View(patient);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            var patient = _patientService.GetPatientById(id);
+
+            return patient == null ? View("NotFound") : View(patient);
+        }
+
+        [HttpPost]
+        public ActionResult DeletePatient(int? id)
+        {
+            _patientService.DeletePatient(id);
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetAllPatients()
+        {
+            var patients = _patientService.GetAllPatients();
+
+            return View(patients);
+        }
+    }
+}
