bb65cc6d2665071e96482e203d9e9ed2ad525ebe
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-03 20:53:20 +0300

    EC-8: updated phone controller.

--- ElectronicCard/EC.Web/Controllers/PhoneController.cs
+++ ElectronicCard/EC.Web/Controllers/PhoneController.cs
@@ -1,5 +1,6 @@
-﻿using System.Web.Mvc;
-using EC.BusinessLogic.Services.Interfaces;
+﻿using EC.BusinessLogic.Services.Interfaces;
+using EC.Entities.Entities;
+using System.Web.Mvc;
 
 namespace EC.Web.Controllers
 {
@@ -12,9 +13,66 @@ namespace EC.Web.Controllers
             _phoneService = phoneService;
         }
 
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult AddPhone()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPhone(Phone phone)
+        {
+            if (ModelState.IsValid)
+            {
+                _phoneService.CreatePhone(phone);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UpdatePhone(int? id)
+        {
+            var phone = _phoneService.GetById(id);
+
+            return View(phone);
+        }
+
+        [HttpPost]
+        public ActionResult UpdatePhone(Phone phone)
+        {
+            if (ModelState.IsValid)
+            {
+                _phoneService.UpdatePhone(phone);
+            }
+
+            return View(phone);
+        }
+
+        [HttpPost]
+        public ActionResult DeletePhone(int? id)
+        {
+            if (id == null)
+            {
+                return View("NotFound");
+            }
+
+            _phoneService.DeletePhone(id);
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UserPhones(int? userId)
+        {
+            var userPhones = _phoneService.GetUserPhones(userId);
+
+            if (userPhones == null)
+            {
+                return View("NotFound");
+            }
+
+            return View(userPhones);
         }
     }
 }
