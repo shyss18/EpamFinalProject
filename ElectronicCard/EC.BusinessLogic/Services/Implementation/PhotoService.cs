15460aa63ecc2067878fc87927d38c9cf704ada5
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-05 02:00:27 +0300

    EC-9: created service and controller for photo.

---
+++ ElectronicCard/EC.BusinessLogic/Services/Implementation/PhotoService.cs
@@ -0,0 +1,41 @@
+﻿using EC.BusinessLogic.Services.Interfaces;
+using EC.DataAccess.Repositories.Interfaces;
+using EC.Entities.Entities;
+
+namespace EC.BusinessLogic.Services.Implementation
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly IPhotoRepository _photoRepository;
+
+        public PhotoService(IPhotoRepository photoRepository)
+        {
+            _photoRepository = photoRepository;
+        }
+
+        public void CreatePhoto(Photo photo)
+        {
+            _photoRepository.Create(photo);
+        }
+
+        public void UpdatePhoto(Photo photo)
+        {
+            _photoRepository.Update(photo);
+        }
+
+        public void DeletePhoto(int? id)
+        {
+            _photoRepository.Delete(id);
+        }
+
+        public Photo GetById(int? id)
+        {
+            return id == null ? null : _photoRepository.GetById(id);
+        }
+
+        public Photo GetUserPhoto(int? userId)
+        {
+            return userId == null ? null : _photoRepository.GetUserPhoto(userId);
+        }
+    }
+}
---
+++ ElectronicCard/EC.Web/Controllers/PhotoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EC.BusinessLogic.Services.Interfaces;
+
+namespace EC.Web.Controllers
+{
+    public class PhotoController : Controller
+    {
+        private readonly IPhotoService _photoService;
+
+        public PhotoController(IPhotoService photoService)
+        {
+            _photoService = photoService;
+        }
+
+        //[HttpPost]
+        //public ActionResult CreatePhoto(HttpPostedFileBase photo)
+        //{
+        //    if (photo != null)
+        //    {
+                
+        //    }
+        //}
+    }
+}
