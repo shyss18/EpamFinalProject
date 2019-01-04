abf814220f8cca426b4668f580bda0e68ad96b86
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-04 10:40:09 +0300

    EC-9: create registration form.

--- ElectronicCard/EC.Web/Controllers/AuthController.cs
+++ ElectronicCard/EC.Web/Controllers/AuthController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Web.Mvc;
+using EC.Web.Models;
 
 namespace EC.Web.Controllers
 {
     public class AuthController : Controller
     {
+        private static bool _isDoctor;
+
         public AuthController()
         {
+
         }
 
         [HttpGet]
@@ -15,9 +19,43 @@ namespace EC.Web.Controllers
         }
 
         [HttpGet]
-        public ActionResult SignUpPatient()
+        public ActionResult SignUp()
         {
+            ViewBag.IsDoctor = _isDoctor;
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(RegistrationModel user)
+        {
+            ViewBag.IsDoctor = _isDoctor;
+
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult CheckForDoctor(string code)
+        {
+            const string verificationCode = "123456";
+
+            if (verificationCode.Equals(code))
+            {
+                _isDoctor = true;
+
+                return RedirectToAction("SignUp");
+            }
+            else
+            {
+                _isDoctor = false;
+
+                return RedirectToAction("SignUp");
+            }
         }
     }
 }
---
+++ ElectronicCard/EC.Web/Models/RegistrationModel.cs
@@ -0,0 +1,48 @@
+﻿using EC.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace EC.Web.Models
+{
+    public class RegistrationModel
+    {
+        [DisplayName("Логин")]
+        public string Login { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Пароль")]
+        public string Password { get; set; }
+
+        [DisplayName("Подтверждение пароля")]
+        public string ConfirmPassword { get; set; }
+
+        public bool IsDoctor { get; set; }
+
+        [DisplayName("Фотография")]
+        public string PhotoPath { get; set; }
+
+        [DisplayName("Имя")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Отчество")]
+        public string MiddleName { get; set; }
+
+        [DisplayName("Фамилия")]
+        public string LastName { get; set; }
+
+        [DisplayName("Место работы")]
+        public string PlaceWork { get; set; }
+
+        [DisplayName("Должность")]
+        public string Position { get; set; }
+
+        [DisplayName("Дата рождения")]
+        public DateTime DateBirth { get; set; }
+
+        [DisplayName("Номер телефона")]
+        public virtual ICollection<Phone> Phones { get; set; }
+    }
+}
