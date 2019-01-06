d45779925a610f2596dd1c5a2f2dab3a1ba511b2
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-07 00:55:41 +0300

    EC-10: added script for hide div blocks.

---
+++ ElectronicCard/EC.Web/Scripts/checkUser.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $("#IsDoctor").change(function () {
+        if ($(this).prop('checked')) {
+            $('#Position').fadeIn(300);
+            $('#PlaceWork').fadeOut().show();
+            $('#DateBirth').fadeOut().show();
+        } else {
+            $('#Position').fadeOut().show();
+            $('#PlaceWork').fadeIn(300);
+            $('#DateBirth').fadeIn(300);
+        }
+    });
+})
