41d95830910d313d9945943e0aabaaa47bf3c9a4
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-07 00:52:04 +0300

    EC-10: hidden id in intities.

--- ElectronicCard/EC.Entities/Entities/Patient.cs
+++ ElectronicCard/EC.Entities/Entities/Patient.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EC.Entities.Entities
 {
@@ -7,14 +9,21 @@ namespace EC.Entities.Entities
     {
         public int UserId { get; set; }
 
+        [DisplayName("Имя")]
         public string FirstName { get; set; }
 
+        [DisplayName("Отчество")]
         public string MiddleName { get; set; }
 
+        [DisplayName("Фамилия")]
         public string LastName { get; set; }
 
+        [DisplayName("Место работы")]
         public string PlaceWork { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayName("Дата рождения")]
         public DateTime DateBirth { get; set; }
 
         public User User { get; set; }
