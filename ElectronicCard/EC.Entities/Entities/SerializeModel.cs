1d462f2a4b9eb031fcfd3672aba6b4e9141c026a
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-08 02:39:56 +0300

    EC-10: created serialzation model for user.

---
+++ ElectronicCard/EC.Entities/Entities/SerializeModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EC.Entities.Entities
+{
+    public class SerializeModel
+    {
+        public string Login { get; set; }
+
+        public IReadOnlyCollection<Role> Roles { get; set; }
+    }
+}
