99c2bfed825ee279d4884a48b8f5b02299282f2d
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-08 02:37:11 +0300

    EC-10: created views and model for edit photo.

---
+++ ElectronicCard/EC.Web/Models/EditPhotoModel.cs
@@ -0,0 +1,16 @@
+﻿using EC.Entities.Entities;
+using System.Web;
+
+namespace EC.Web.Models
+{
+    public class EditPhotoModel
+    {
+        public int Id { get; set; }
+
+        public Photo UserPhoto { get; set; }
+
+        public int UserId { get; set; }
+
+        public HttpPostedFileBase Image { get; set; }
+    }
+}
