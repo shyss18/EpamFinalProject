16a3071592296b7ef44cb9a6453bd349e57361af
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-09 01:32:10 +0300

    EC-10: update database scripts.

--- DataBase/DataBaseScripts/StoredProcedures/Users/GetAllUsers.sql
+++ DataBase/DataBaseScripts/StoredProcedures/Users/GetAllUsers.sql
@@ -4,7 +4,7 @@ GO
 CREATE PROC dbo.GetAllUsers
 AS
 BEGIN
-   SELECT * FROM dbo.Users
+   SELECT Users.Id, Users.Login, Users.Email, Users.Password, Users.IsDoctor, Doctors.UserId, Doctors.FirstName, Doctors.MiddleName, Doctors.LastName, Doctors.Position, Patients.UserId AS PatientId, Patients.FirstName AS PatientFirstName, Patients.MiddleName AS PatientMiddleName, Patients.LastName AS PatientLastName, Patients.DateBirth, Patients.PlaceWork, Photo.Id, Photo.Image, Photo.ImageType, Photo.UserId FROM dbo.Users
    LEFT JOIN dbo.Doctors ON dbo.Doctors.UserId = dbo.Users.Id
    LEFT JOIN dbo.Patients ON dbo.Patients.UserId = dbo.Users.Id
    LEFT JOIN dbo.Photo ON dbo.Photo.UserId = dbo.Users.Id
