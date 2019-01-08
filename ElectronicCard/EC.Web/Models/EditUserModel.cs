878c125407a1a4db3a2e0b811ef1d18d41f0a40f
Sergey Petrochenko <proffsneip@gmail.com>
2019-01-08 18:51:40 +0300

    EC-10:created update user from admin panel.

---
+++ ElectronicCard/EC.Web/Models/EditUserModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace EC.Web.Models
+{
+    public class EditUserModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [DisplayName("Имя")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Отчество")]
+        public string MiddleName { get; set; }
+
+        [DisplayName("Фамилия")]
+        public string LastName { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public bool IsDoctor { get; set; }
+
+        [DisplayName("Должность")]
+        public string Position { get; set; }
+
+        [DisplayName("Место работы")]
+        public string PlaceWork { get; set; }
+
+        [DisplayName("Дата рождения")]
+        public DateTime DateBirth { get; set; }
+
+        [DisplayName("Роли")]
+        public int[] Roles { get; set; }
+
+        [DisplayName("Пациенты")]
+        public int[] Patients { get; set; }
+
+        [DisplayName("Доктора")]
+        public int[] Doctors { get; set; }
+    }
+}
